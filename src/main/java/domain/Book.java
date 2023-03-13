a6487efa7a85b71e913a8163f3d60086409bc249
tatiana dalvadiants <eparis@atlassian.com>
2023-03-13 14:47:28 +0400

    git ignore added

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -3,6 +3,7 @@ package domain;
 public class Book extends Product {
     private String author;
 
+
     public Book(int id, String name, int price) {
         super(id, name, price);
         this.author = getAuthor();
@@ -14,6 +15,13 @@ public class Book extends Product {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+    public boolean matches(String search) {
+        if (super.matches(search)) { // вызов метода matches в версии описанной в Product
+            return true;
+        }
+        if (super.matches(search)) { // вызов метода matches в версии описанной в Product
+            return false;...;
     }
 }
 
